3876192ce46d8b10a9ac935fcbcefe635ae39ab9
Franco Frizzo <francofrizzo@gmail.com>
2026-02-08 20:51:16 -0300

    Add label formula

--- Formula/label.rb
+++ Formula/label.rb
@@ -11,22 +11,22 @@ class Label < Formula
 
   resource "bleak" do
     url "https://files.pythonhosted.org/packages/fc/a5/47009938c89176d199b6d51e97475a1ca9fc33ae79f21ae41ddb49c4e32e/bleak-0.22.3.tar.gz"
-    sha256 "dbddc4ef7dab4488a064570881104bcc2c1e6cb8ad11a26f16dc8d00ee732eca"
+    sha256 "bc9f4b8ad7f76f409db0c451c41e0e52f00d3e861d8ff90e75f2b98e8b6e06ae"
   end
 
   resource "Pillow" do
     url "https://files.pythonhosted.org/packages/a5/26/0d95c04c868f6bdb0c447e3ee2de5564411845e36a858cfd63766bc7b563/pillow-11.0.0.tar.gz"
-    sha256 "dbddc4ef7dab4488a064570881104bcc2c1e6cb8ad11a26f16dc8d00ee732eca"
+    sha256 "72bacbaf24ac003fea9bff9837d1eedb6088758d41e100c1552930151f677739"
   end
 
   resource "numpy" do
     url "https://files.pythonhosted.org/packages/65/6e/09db70a523a96d25e115e71cc56a6f9031e7b8cd166c1ac8438307c14058/numpy-1.26.4.tar.gz"
-    sha256 "dbddc4ef7dab4488a064570881104bcc2c1e6cb8ad11a26f16dc8d00ee732eca"
+    sha256 "2a02aba9ed12e4ac4eb3ea9421c420301a0c6460d9830d74a9df87efa4912010"
   end
 
   resource "opencv-python" do
     url "https://files.pythonhosted.org/packages/4a/e7/b70a2d9ab205110d715906fc8ec83fbb00404aeb3a37a0654fdb68e5d87b/opencv-python-4.11.0.86.tar.gz"
-    sha256 "dbddc4ef7dab4488a064570881104bcc2c1e6cb8ad11a26f16dc8d00ee732eca"
+    sha256 "4d8a7e45bf48302e838510d621dbfc53c1e2ce0e3ab2709afdaa70e6cd82e93c"
   end
 
   def install
