67f2ff1f53559ac0dad68c6b194d5f184a4c66d5
Franco Frizzo <francofrizzo@gmail.com>
2026-06-01 15:41:39 -0300

    wt v0.11.1

--- Formula/wt.rb
+++ Formula/wt.rb
@@ -1,8 +1,8 @@
 class Wt < Formula
   desc "Git worktree manager with GitHub PR and CI status integration"
   homepage "https://github.com/francofrizzo/wt"
-  url "https://github.com/francofrizzo/wt/archive/refs/tags/v0.11.0.tar.gz"
-  sha256 "44d90731f553c9128521841f4935d4e9d2e688d55e0a71e30d4d7395782cfed8"
+  url "https://github.com/francofrizzo/wt/archive/refs/tags/v0.11.1.tar.gz"
+  sha256 "76a51fbcaf56ba8bfc5285a9b93ed2957743947b87d2beb414d282e3284eac3b"
   license "MIT"
 
   depends_on "jq"
